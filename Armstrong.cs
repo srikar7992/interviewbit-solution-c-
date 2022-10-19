c26bf210668323bb1c150d0721043c284ead17dd
srikar7992 <101961313+srikar7992@users.noreply.github.com>
2022-10-19 23:50:08 +0530

    Update Armstrong.cs

--- Armstrong.cs
+++ Armstrong.cs
@@ -3,10 +3,6 @@ using System.IO;
 
 class MAIN  {
     public static void Main(string[] args) {
-        // YOUR CODE GOES HERE
-        // Please take input and print output to standard input/output (stdin/stdout)
-        // DO NOT USE ARGUMENTS FOR INPUT
-        // E.g. 'StreamReader' for input & 'StreamWriter' for output
         int input = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(1);
         for(int i = 150; i <= input; i++){
